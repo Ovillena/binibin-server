cdcd989bc92cef8339e5b34e5a5be17c4c16c1a2
Octavio Villena <octaviovillena@hotmail.com>
2021-10-25 22:18:41 -0700

    initial commit

---
+++ databaseConnection.js
@@ -0,0 +1,30 @@
+const { Pool } = require("pg");
+
+require("dotenv").config();
+
+const is_heroku = process.env.IS_HEROKU || false;
+
+const pgPool = new Pool({
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: process.env.PG_PWD,
+  database: "playground",
+});
+
+const pgHerokuPool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+if (is_heroku) {
+  var database = pgHerokuPool;
+} else {
+  //   var database = mysql.createPool(dbConfigLocal);
+  // var database = pgPool;
+  var database = pgPool;
+}
+
+module.exports = database;
---
+++ routes/router.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+const database = include("databaseConnection");
+const dbModel = include("databaseAccessLayer");
+
+router.get("/", (req, res) => {
+  console.log("page hit");
+  database.connect(function (err, dbConnection) {
+    if (err) {
+      res.send("Error connecting to PostgreSQL");
+      console.log("Error connecting to PostgreSQL");
+      console.log(err);
+    } else {
+      dbModel.getAllUsers((err, result) => {
+        if (err) {
+          res.send("Error reading from PostgreSQL");
+          console.log("Error reading from PostgreSQL");
+          console.log(err);
+        } else {
+          //success
+          res.json(result.rows);
+
+          //Output the results of the query to the Heroku Logs
+          console.log(result.rows);
+        }
+      });
+      dbConnection.release();
+    }
+  });
+});
+
+module.exports = router;
