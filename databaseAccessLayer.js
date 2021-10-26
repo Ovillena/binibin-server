fba327da0dd873541494e9f8b6c0f3565ba31189
Octavio Villena <octaviovillena@hotmail.com>
2021-10-26 00:55:33 -0700

    eslint prettier stuff

--- databaseAccessLayer.js
+++ databaseAccessLayer.js
@@ -1,70 +1,75 @@
 const database = include('/databaseConnection');
 
 function getAllUsers(callback) {
-  let sqlQuery = 'SELECT web_user_id, first_name, last_name, email FROM users';
-  database.query(sqlQuery, (err, results, fields) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      console.log(results);
-      callback(null, results);
-    }
-  });
+    let sqlQuery =
+        'SELECT web_user_id, first_name, last_name, email FROM users';
+    database.query(sqlQuery, (err, results, fields) => {
+        if (err) {
+            callback(err, null);
+        } else {
+            console.log(results);
+            callback(null, results);
+        }
+    });
 }
 
 const passwordPepper = 'SeCretPeppa4MySal+';
 function addUser(postData, callback) {
-  let sqlInsertSalt =
-    'INSERT INTO users (first_name, last_name, email, password_salt)VALUES (:first_name, :last_name, :email, sha2(UUID(),512));';
-  let params = {
-    first_name: postData.first_name,
-    last_name: postData.last_name,
-    email: postData.email,
-  };
-  console.log(sqlInsertSalt);
-  database.query(sqlInsertSalt, params, (err, results, fields) => {
-    if (err) {
-      console.log(err);
-      callback(err, null);
-    } else {
-      let insertedID = results.insertId;
-      let updatePasswordHash =
-        'UPDATE users SET password_hash = sha2(concat(:password,:pepper,password_salt),512) WHERE web_user_id = :userId;';
-      let params2 = {
-        password: postData.password,
-        pepper: passwordPepper,
-        userId: insertedID,
-      };
-      console.log(updatePasswordHash);
-      database.query(updatePasswordHash, params2, (err, results, fields) => {
+    let sqlInsertSalt =
+        'INSERT INTO users (first_name, last_name, email, password_salt)VALUES (:first_name, :last_name, :email, sha2(UUID(),512));';
+    let params = {
+        first_name: postData.first_name,
+        last_name: postData.last_name,
+        email: postData.email,
+    };
+    console.log(sqlInsertSalt);
+    database.query(sqlInsertSalt, params, (err, results, fields) => {
         if (err) {
-          console.log(err);
-          callback(err, null);
+            console.log(err);
+            callback(err, null);
         } else {
-          console.log(results);
-          callback(null, results);
+            let insertedID = results.insertId;
+            let updatePasswordHash =
+                'UPDATE users SET password_hash = sha2(concat(:password,:pepper,password_salt),512) WHERE web_user_id = :userId;';
+            let params2 = {
+                password: postData.password,
+                pepper: passwordPepper,
+                userId: insertedID,
+            };
+            console.log(updatePasswordHash);
+            database.query(
+                updatePasswordHash,
+                params2,
+                (err, results, fields) => {
+                    if (err) {
+                        console.log(err);
+                        callback(err, null);
+                    } else {
+                        console.log(results);
+                        callback(null, results);
+                    }
+                }
+            );
         }
-      });
-    }
-  });
+    });
 }
 
 function deleteUser(webUserId, callback) {
-  let sqlDeleteUser = 'DELETE FROM users WHERE web_user_id = :userID';
-  let params = { userID: webUserId };
-  console.log(sqlDeleteUser);
-  database.query(sqlDeleteUser, params, (err, results, fields) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      console.log(results);
-      callback(null, results);
-    }
-  });
+    let sqlDeleteUser = 'DELETE FROM users WHERE web_user_id = :userID';
+    let params = { userID: webUserId };
+    console.log(sqlDeleteUser);
+    database.query(sqlDeleteUser, params, (err, results, fields) => {
+        if (err) {
+            callback(err, null);
+        } else {
+            console.log(results);
+            callback(null, results);
+        }
+    });
 }
 
 module.exports = {
-  getAllUsers,
-  addUser,
-  deleteUser,
+    getAllUsers,
+    addUser,
+    deleteUser,
 };
--- databaseConnection.js
+++ databaseConnection.js
@@ -1,28 +1,28 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg');
 
-require("dotenv").config();
+require('dotenv').config();
 
 const is_heroku = process.env.IS_HEROKU || false;
 
 const pgPool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  port: 5432,
-  password: process.env.PG_PWD,
-  database: "playground",
+    host: 'localhost',
+    user: 'postgres',
+    port: 5432,
+    password: process.env.PG_PWD,
+    database: 'playground',
 });
 
 const pgHerokuPool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 if (is_heroku) {
-  var database = pgHerokuPool;
+    var database = pgHerokuPool;
 } else {
-  var database = pgPool;
+    var database = pgPool;
 }
 
 module.exports = database;
