b27a2bed4b6b54e0a27c2bd47bf591e18838aad9
Megan Kuo <megan.kuo@live.com>
2021-11-10 15:20:54 -0800

    fixed formatting

--- routes/auth.js
+++ routes/auth.js
@@ -1,31 +1,34 @@
 const router = require('express').Router();
-const { ensureAuthenticated } = require("../middleware/checkAuth")
+const { ensureAuthenticated } = require('../middleware/checkAuth');
 
-const passport = require("../middleware/passport");
-
+const passport = require('../middleware/passport');
 
 router.get('/sadness', (req, res) => {
-  res.send("sadness. try again")
-})
+    res.send('sadness. try again');
+});
 
 router.get('/login', (req, res) => {
-  res.send("At login page. Post username and password to login.")
-})
-router.post("/login",
-  passport.authenticate("local", {
-    failureRedirect: '/auth/sadness',
+    res.send('At login page. Post username and password to login.');
+});
 
-  }), (req, res) => {
-    // res.redirect("/");
-    res.redirect('/auth/hiddenpage')
-  }
-)
-router.get("/hiddenpage", ensureAuthenticated, (req, res) => {
-  res.send("At hidden page for logged in users only. You are logged in")
-})
-router.get("/logout", (req, res) => {
-  req.logout();
-  res.send("you logged out")
-})
+router.post(
+    '/login',
+    passport.authenticate('local', {
+        failureRedirect: '/auth/sadness',
+    }),
+    (req, res) => {
+        // res.redirect("/");
+        res.redirect('/auth/hiddenpage');
+    }
+);
+
+router.get('/hiddenpage', ensureAuthenticated, (req, res) => {
+    res.send('At hidden page for logged in users only. You are logged in');
+});
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.send('you logged out');
+});
 
 module.exports = router;
