452fa55c101fb658e8ac27f1aba6f25378eb578f
Megan Kuo <megan.kuo@live.com>
2021-11-10 16:05:09 -0800

    send back user json info

--- routes/auth.js
+++ routes/auth.js
@@ -11,16 +11,43 @@ router.get('/login', (req, res) => {
     res.send('At login page. Post username and password to login.');
 });
 
-router.post(
-    '/login',
-    passport.authenticate('local', {
-        failureRedirect: '/auth/sadness',
-    }),
-    (req, res) => {
-        // res.redirect("/");
-        res.redirect('/auth/hiddenpage');
-    }
-);
+// router.post(
+//     '/login',
+//     passport.authenticate('local', {
+//         // failureRedirect: '/auth/sadness',
+//         failureRedirect: { username: '' },
+//     }),
+//     (req, res) => {
+//         // res.redirect("/");
+//         // res.redirect('/auth/hiddenpage');
+//         res.json({ username: req.user });
+//     }
+// );
+
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.json({ message: 'user not found', error: err });
+        }
+        req.logIn(user, function (err) {
+            if (err) {
+                return next(err);
+            }
+            return res.json({
+                account_id: user.account_id,
+                username: user.username,
+                email: user.email,
+                school_id: user.school_id,
+                admin_account_id: user.admin_account_id,
+                display_name: user.display_name,
+                is_admin: user.is_admin,
+            });
+        });
+    })(req, res, next);
+});
 
 router.get('/hiddenpage', ensureAuthenticated, (req, res) => {
     res.send('At hidden page for logged in users only. You are logged in');
