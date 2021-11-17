bae783ccea9aa184c245a1583361edc166638aa4
Octavio Villena <octaviovillena@hotmail.com>
2021-11-16 20:24:38 -0800

    secure true cookie

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(
         saveUninitialized: false,
         cookie: {
             httpOnly: true,
-            secure: false,
+            secure: true,
             maxAge: 24 * 60 * 60 * 1000,
             sameSite: 'none',
         },
