0445ec28d64295ac603eebd8275a9db435810f6e
Octavio Villena <octaviovillena@hotmail.com>
2021-11-17 01:12:02 -0800

    app set headers. try to prevent cors

--- app.js
+++ app.js
@@ -19,6 +19,21 @@ app.use(
     })
 );
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://binibin.vercel.app');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-Requested-With,content-type'
+    );
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
 // Testing ability to connect to db
 database.connect((err, dbConnection) => {
     if (!err) {
