e2e41c98bbd7a5bfce81fb54d08870f6b2910f44
Octavio Villena <octaviovillena@hotmail.com>
2021-11-16 19:25:43 -0800

    lax samesite

--- app.js
+++ app.js
@@ -40,9 +40,9 @@ app.use(
         saveUninitialized: false,
         cookie: {
             httpOnly: true,
-            secure: true,
+            secure: false,
             maxAge: 24 * 60 * 60 * 1000,
-            sameSite: 'none',
+            sameSite: 'lax',
         },
     })
 );
