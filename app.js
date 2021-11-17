4e9d85da1f167850938949323dcb805131375a06
Octavio Villena <octaviovillena@hotmail.com>
2021-11-16 19:00:54 -0800

    wildcard cors

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 
 app.use(
     cors({
-        origin: ['http://localhost:3000', 'https://binibin.vercel.app'],
+        origin: '*',
         optionsSuccessStatus: 200,
         exposedHeaders: ['set-cookie'],
         credentials: true,
